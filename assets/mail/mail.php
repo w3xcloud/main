3def84844a5fc3d13462f7aa56bec2d548e16ee8
W3X Cloud <137123639+w3xcloud@users.noreply.github.com>
2023-06-20 22:14:51 -0300

    Add files via upload

---
+++ assets/mail/mail.php
@@ -0,0 +1,22 @@
+<?
+$name = $_POST{'name'};
+$email = $_POST{'email'};
+$subject = $_POST{'subject'};
+$phone = $_POST{'phone'};
+$message = $_POST['message'];
+
+$email_message = "
+
+Name: ".$name."
+Email: ".$email."
+Subject: ".$subject."
+Phone: ".$phone."
+Message: ".$message."
+
+";
+
+mail ("w3xcloud@gmail.com" , "New Message", $email_message);
+header("location: ../../index.html");
+?>
+
+
